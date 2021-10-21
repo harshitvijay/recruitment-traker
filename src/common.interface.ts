d4c02590832cbf82801842f53c3d59695c397c59
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-21 19:07:45 +0530

    Common Interface Updated

---
+++ src/common.interface.ts
@@ -0,0 +1,12 @@
+export interface FieldsInterface {
+  [name: string]: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+export interface ErrorInterface {
+  [name: string]: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
