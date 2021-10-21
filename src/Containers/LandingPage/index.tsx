8728d2d81ccc5aa679851d79885690c8dce18e61
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-21 19:10:35 +0530

    Updated Landing Page Container

--- src/Containers/LandingPage/index.tsx
+++ src/Containers/LandingPage/index.tsx
@@ -1,8 +1,21 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "src/Components/Login";
 import Signup from "src/Components/Signup";
 
 const LandingPage: FC = () => {
-  return <Signup />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path={`/signup`}>
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default LandingPage;
