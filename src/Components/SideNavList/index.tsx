4cea0a20f8bcefe1d2bbcf2d6717b1b83219ea9a
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-25 15:22:27 +0530

    Added Side Nav List Component

---
+++ src/Components/SideNavList/index.tsx
@@ -0,0 +1,50 @@
+import { FC } from "react";
+
+const SideNavList: FC = () => {
+  return (
+    <div className="position-sticky pt-md-5">
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-home"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+            <span className="ms-2">Dashboard</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="1em"
+              height="1em"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 16 16"
+            >
+              <g fill="currentColor">
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
+              </g>
+            </svg>
+            <span className="ms-2">Tables</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideNavList;
