4f618e9ba1afd8630bf82e9a9433d5ce7be2c06b
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-25 19:08:02 +0530

    Updated Dashboard Content Card Component

--- src/Components/DashboardContentCard/index.tsx
+++ src/Components/DashboardContentCard/index.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
+import { CandidateProfileInterface } from "src/common.interface";
 
-const DashboardContentCard: FC = () => {
+const DashboardContentCard: FC<{ data: CandidateProfileInterface }> = ({
+  data,
+}) => {
   return (
-    <div className="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0">
+    <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-4 text-center">
       <div className="card">
-        <h5 className="card-header">Customers</h5>
+        <h5 className="card-header">{data.name}</h5>
         <div className="card-body">
-          <h5 className="card-title">345k</h5>
-          <p className="card-text">Feb 1 - Apr 1, United States</p>
-          <p className="card-text text-success">
-            18.2% increase since last month
+          <h5 className="card-title">Experience: {data.experience}</h5>
+          <p className="card-text">{data.technology}</p>
+          <p className="card-text text-danger">
+            Current Company: Deqode Solution
           </p>
+          <a className="card-link" href="#">
+            Resume
+          </a>
         </div>
       </div>
     </div>
