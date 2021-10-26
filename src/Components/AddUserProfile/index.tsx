2f9ec1db69ce2c2d42a5c78a40c75f1c47ab02e4
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-27 02:07:14 +0530

    Added User Profile Component

---
+++ src/Components/AddUserProfile/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import DashboardHeader from "../DashboardHeader";
+
+const AddUserProfile: FC = () => {
+  return (
+    <div className="dash-content">
+      <DashboardHeader title="Add Profile" mainHeading="Add Users" />
+    </div>
+  );
+};
+
+export default AddUserProfile;
