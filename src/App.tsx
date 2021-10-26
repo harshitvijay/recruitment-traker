5339fda3c438ac562501fe2a220db9bfc4ff50fe
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-27 02:06:02 +0530

    Updated App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import { FC } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./Containers/LandingPage";
-import DashboardPage from "./Containers/DashboardPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const App: FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path={`/dashboard`}>
-          <DashboardPage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <LandingPage />;
 };
 
 export default App;
