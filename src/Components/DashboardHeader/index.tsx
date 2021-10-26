599b3e11c99ee154e6bf6101dc64bc136a93e166
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-27 02:08:31 +0530

    Updated Dashboard Header Component

--- src/Components/DashboardHeader/index.tsx
+++ src/Components/DashboardHeader/index.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 
-const DashboardHeader: FC = () => {
+const DashboardHeader: FC<{
+  title: string;
+  mainHeading: string;
+}> = ({ title, mainHeading }) => {
   return (
     <div>
       <nav aria-label="breadcrumb">
@@ -9,16 +12,17 @@ const DashboardHeader: FC = () => {
             <a href="#">Home</a>
           </li>
           <li className="breadcrumb-item active" aria-current="page">
-            Overview
+            {title}
           </li>
         </ol>
       </nav>
-      <h1 className="h2">Dashboard</h1>
+      <h1 className="h2">{mainHeading}</h1>
       <p className="text-secondary">
         The competition to hire the best will increase in the years ahead.
         Companies that give extra flexibility to their employees will have the
         edge in this area. (Bill Gates)
       </p>
+      <h3 className="text-secondary">{title}</h3>
     </div>
   );
 };
