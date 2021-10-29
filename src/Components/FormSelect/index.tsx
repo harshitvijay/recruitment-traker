bf2b3eaae04982c434c9be3615ba1c37232151a4
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-29 12:00:36 +0530

    Added Form Select Component

---
+++ src/Components/FormSelect/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import Form from "react-bootstrap/Form";
+import FormSelectInterface from "./FormSelect.interface";
+
+const FormSelect: FC<FormSelectInterface> = ({
+  label,
+  error,
+  name,
+  fields,
+  setFields,
+  option,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFields({ ...fields, [e.target.name]: e.target.value });
+  };
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label>
+        {label}
+        <span className="text-danger">*</span>
+      </Form.Label>
+      <Form.Select onChange={handleChange} name={name} defaultValue={option[0]}>
+        {option.map((val: string, index: number) => (
+          <option key={index} value={val}>
+            {val}
+          </option>
+        ))}
+      </Form.Select>
+      <Form.Text className="text-danger">{error}</Form.Text>
+    </Form.Group>
+  );
+};
+
+export default FormSelect;
