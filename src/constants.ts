37fdfa5e5ef210ea8b3c1bbbbea12208a9372565
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-29 11:52:01 +0530

    Updated Constant File

--- src/constants.ts
+++ src/constants.ts
@@ -39,13 +39,136 @@ export const LoginFormInputData = [
     name: "password",
   },
 ];
+
+export const addProfileFormSelect = [
+  {
+    label: "Gender",
+    name: "gender",
+    option: ["Male", "Female", "Other"],
+  },
+  {
+    label: "Joining Location",
+    name: "location",
+    option: ["Indore", "Pune", "Hyderabad", "Ahemdabad"],
+  },
+  {
+    label: "Hunted By",
+    name: "hunted_by",
+    option: [
+      "Aman Yadva",
+      "Vishal Soni",
+      "Ayush Saini",
+      "Samiksha Agrawal",
+      "Ayushi Sengar",
+      "Mohini Bansal",
+      "Arpita Saxena",
+    ],
+  },
+];
 export const initialFields = {
   name: "",
   email: "",
   password: "",
   confirmPassword: "",
 };
+export const userProfileInitialFields = {
+  name: "",
+  email: "",
+  gender: "",
+  experience: "",
+  current_salary: "",
+  expected_ctc: "",
+  notice_period: "",
+  current_status: "",
+  technology: "",
+  contact_no: "",
+  head_hunted_by: "",
+  source_of_hire: "",
+};
+
+export const CandidatePersonalInfo = [
+  {
+    label: "Name",
+    type: "text",
+    placeHolder: "Enter Name",
+    name: "name",
+  },
+  {
+    label: "Email Address",
+    type: "email",
+    placeHolder: "Enter Email",
+    name: "email",
+  },
+
+  {
+    label: "Contact Number",
+    type: "tel",
+    placeHolder: "Enter Number",
+    name: "contact_no",
+  },
+];
+
+export const tableHeadings = [
+  {
+    label: "S.No",
+    value: "S.no",
+  },
+  {
+    label: "name",
+    value: "Name",
+  },
+  {
+    label: "email",
+    value: "Email",
+  },
+  {
+    label: "contact number",
+    value: "Contact Number",
+  },
+  {
+    label: "technology",
+    value: "Technology",
+  },
+  {
+    label: "current_status",
+    value: "Current_Status",
+  },
+  {
+    label: "experience",
+    value: "Experience",
+  },
+  {
+    label: "current_salary",
+    value: "Current_Salary",
+  },
+  {
+    label: "expected_ctc",
+    value: "Excpected_CTC",
+  },
+];
+
+export const dropDownConstants = [
+  {
+    label: "five",
+    value: "5",
+  },
+  {
+    label: "ten",
+    value: "10",
+  },
+  {
+    label: "fifteen",
+    value: "15",
+  },
+];
+export const header = {
+  name: "No Item Found",
+  page: "Per Page",
+  loading: "Loading...",
+};
+
 export const loginUrl = "https://rms-01.herokuapp.com/api/login";
 export const signupUrl = "https://rms-01.herokuapp.com/api/register";
 export const candidateProfileURL =
   "https://rms-01.herokuapp.com/api/get-profiles";
+export const userProfileList = "https://rms-01.herokuapp.com/api/get-profiles";
