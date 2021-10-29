6196790581e5bad9506c2df1ea5bdb08e4a9a9dd
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-29 11:59:39 +0530

    Updated Dash Board Table Content Component

--- src/Components/DashboardTableContent/index.tsx
+++ src/Components/DashboardTableContent/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import TableMain from "../ComponentTable/TableMain";
 import DashboardHeader from "../DashboardHeader";
 
 const DashboardTableContent: FC = () => {
   return (
     <div className="dash-content">
       <DashboardHeader title="User Details" mainHeading="Tables" />
+      <TableMain />
     </div>
   );
 };
