123e9486db4469fbb4f34098d46a723e2337c145
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-11-10 14:10:07 +0530

    Updated Service File

--- src/service.ts
+++ src/service.ts
@@ -52,3 +52,23 @@ export const getCandidateProfile = async (url: string) => {
   const result = await response.json();
   return result;
 };
+const getCitis = async () => {
+  const headers = new Headers();
+  headers.append(
+    "X-CSCAPI-KEY",
+    "cGFJdmpIc2tlaVV0QmoxemF0NnpqS0xDSTJtOW1LNU1tR3RBdGpiag=="
+  );
+
+  const requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+  const response = await fetch(
+    "https://api.countrystatecity.in/v1/countries/IN/cities",
+    requestOptions
+  );
+  const result = await response.json();
+  console.log(result);
+};
+getCitis();
