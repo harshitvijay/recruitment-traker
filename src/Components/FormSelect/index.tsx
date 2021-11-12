be0b5bdb58c962238775fd7e05489a09eea25501
ankita sharma <Anksharma@deq.com>
2021-11-12 17:09:00 +0530

    Updated FormSelect Component

--- src/Components/FormSelect/index.tsx
+++ src/Components/FormSelect/index.tsx
@@ -13,13 +13,19 @@ const FormSelect: FC<FormSelectInterface> = ({
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setFields({ ...fields, [e.target.name]: e.target.value });
   };
+  console.log("field select", fields[name]);
   return (
     <Form.Group className="mb-3">
       <Form.Label>
         {label}
         <span className="text-danger">*</span>
       </Form.Label>
-      <Form.Select onChange={handleChange} name={name} defaultValue={option[0]}>
+      <Form.Select
+        onChange={handleChange}
+        name={name}
+        // defaultValue={option[0]}
+        value={fields[name]}
+      >
         {option.map((val: string, index: number) => (
           <option key={index} value={val}>
             {val}
