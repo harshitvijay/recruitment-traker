9cced6b23d5bb25eaf4e7c9aca719260c90431c2
ankita sharma <ankita sharma>
2021-11-29 01:11:16 +0530

    Updated Common Interference Component

--- src/common.interface.ts
+++ src/common.interface.ts
@@ -40,5 +40,44 @@ export type DropDownTypes = {
   label: string;
   value: string;
 };
+export type skillSetType = {
+  id: number;
+  name: string;
+  type: string;
+  total_marks: number;
+};
 
 export type Event = {};
+
+export type UserProfileType = {
+  id: number;
+  name: string;
+  email: string;
+  experience: string;
+  current_salary: string;
+  expected_ctc: string;
+  notice_period: string;
+  current_status: string;
+  technology: string;
+  contact_no: string;
+  head_hunted_by: string;
+  source_of_hire: string;
+  created_at: string;
+  updated_at: string;
+};
+export type UserProfileUpdationField = {
+  id: number;
+  name: string;
+  email: string;
+  experience: string;
+  current_salary: string;
+  expected_ctc: string;
+  notice_period: string;
+  current_status: string;
+  technology: string;
+  contact_no: string;
+  head_hunted_by: string;
+  source_of_hire: string;
+  created_at: string;
+  updated_at: string;
+};
